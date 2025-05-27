7b81bfca7b563f9f1f4c66db691c0359bbe71a72
Shreya7078 <shreyajain7078@gmail.com>
2025-05-27 14:30:20 +0530

    Remove Services component and update Services2 for improved layout and styling

--- src/Components/Services2.jsx
+++ src/Components/Services2.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import cooking from '../assets/cooking.jpg';
 function Services2(props) {
 
 
@@ -7,7 +6,7 @@ function Services2(props) {
     <>
         <div className='pb-10'>
             <h1 className='text-5xl text-[#00246B] font-bold ml-42 mb-2 mt-8'>Services</h1>
-            <p className=" text-xl font-bold ml-40 text-black">
+            <p className=" text-xl font-bold ml-42 text-black">
                 We offer a wide range of services to meet your needs.
             </p>
             <div className='flex w-[80%] flex-wrap justify-center gap-15 mx-auto mt-10'>
