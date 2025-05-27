70695ed1ad6546692af7bc3d9164a0add3c286d7
Shreya7078 <shreyajain7078@gmail.com>
2025-05-27 17:32:23 +0530

    Fix alignment of the Services heading

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -3,7 +3,7 @@ function Services(props) {
 	return (
 		<>
 			<div className='pb-10'>
-				<h1 className='text-5xl text-[#00246B] font-bold ml-42 mb-4 mt-8'>Services</h1>
+				<h1 className='text-5xl text-[#00246B] font-bold ml-41 mb-4 mt-8'>Services</h1>
 				<p className=" text-xl font-bold ml-42 text-black"> 
 					We offer a wide range of services to meet your needs.
 				</p>
